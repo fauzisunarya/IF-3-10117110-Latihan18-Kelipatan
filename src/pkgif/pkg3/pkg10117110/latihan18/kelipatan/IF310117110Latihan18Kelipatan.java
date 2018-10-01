84f723df0422f3182918c6fc12320ee737928e8c
fauzisunarya <fauzialdzikirullah@gmail.com>
2018-10-01 18:29:29 +0700

    first commit

---
+++ src/pkgif/pkg3/pkg10117110/latihan18/kelipatan/IF310117110Latihan18Kelipatan.java
@@ -0,0 +1,25 @@
+
+package pkgif.pkg3.pkg10117110.latihan18.kelipatan;
+
+/**
+ *
+ * @author
+ * Nama : Fauzi Sunarya
+ * Kelas : IF-3
+ * NIM : 10117110
+ * Deskripsi Program : Program Jenis ini Berisi Program Untuk Menampilkan
+ * Bilangan Kelipatan 3.5
+ */
+
+public class IF310117110Latihan18Kelipatan {
+
+    public static void main(String[] args) {
+        double i = 3.5 ;
+        do {
+            System.out.println(i);
+            i = i + 3.5;
+        }while (i <= 35);
+ 
+    }
+    
+}
